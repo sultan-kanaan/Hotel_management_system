24379b3e84c6fd2201dba5feefead72666c31d4a
Sultan Kanaan <98963963+sultan-kanaan@users.noreply.github.com>
2022-05-07 14:05:03 +0300

    Add Some Edit

--- Hotel management system/Models/DTOs/RegisterUserDTO.cs
+++ Hotel management system/Models/DTOs/RegisterUserDTO.cs
@@ -8,13 +8,16 @@ namespace Hotel_management_system.Models.DTOs
 {
     public class RegisterUserDTO
     {
-        [Required]
+        [Required(ErrorMessage ="Please Enter The UserName")]
+        [StringLength(20,MinimumLength =3,ErrorMessage ="invalid UserName")]
         public string Username { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please Enter The Passwoed")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please Enter The EmailAddress")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         public string PhoneNumber { get; set; }
